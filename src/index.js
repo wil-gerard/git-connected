067ac42906273f02b42fb72bd7355375d72a5aa2
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-11-04 03:34:24 -0400

    still experimenting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'client/src/index.css';
-import App from 'client/src/App';
+import 'client/index.css';
+import App from 'client/App';
 
 ReactDOM.render(
   <React.StrictMode>
