63c67601316111c4b441af14a54133fb9ed0cb5c
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-11-06 17:53:50 -0400

    fix: renamed main file to more appopriate 'startServer.js'

--- server/startServer.js
+++ server/startServer.js
@@ -3,4 +3,4 @@ const { NODE_ENV, PORT } = process.env;
 require('dotenv').config({ path: path.resolve(__dirname, `../config/.env.${process.env.NODE_ENV}`) });
 const createAndSetupServer = require('./serverSetup');
 
-const server = createAndSetupServer(NODE_ENV,PORT);
+createAndSetupServer(NODE_ENV,PORT);
