43aa7f13e32c67fe694ee2e5722fa8a21254f88b
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-11-06 02:25:44 -0400

    fix: restructured server setup to be modular and testable with no leftover side effects

--- tests/RootComponent.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import RootComponent from '../src/RootComponent';
-
-test('renders title', () => {
-  render(<RootComponent />);
-  const linkElement = screen.getByText(/GitToKnowEachOther/i);
-  expect(linkElement).toBeInTheDocument();
-});
