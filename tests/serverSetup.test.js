f64a0b9f58952e28a3de580f9face03cf80ac5fa
Wil Gerard <williamrgerard@gmail.com>
2021-11-09 11:48:13 -0600

    add: truthy and isobject test for listenObject

--- tests/serverSetup.test.js
+++ tests/serverSetup.test.js
@@ -10,23 +10,36 @@ describe("serverSetup.js -> createAndSetupServer()", () => {
   //but that string has other unneeded data in it. thankfully the port comes at the end of the string and a colon comes before it
   const connectionKeyArray = serverAndListenObjects.listenObject._connectionKey.split(":");
   const usedPort = connectionKeyArray[connectionKeyArray.length - 1];
-
+  
   test('the server object is truthy', () => {
     expect(serverAndListenObjects.serverObject).toBeTruthy();
   });
+
   test('the server object is an object', () => {
     expect(serverAndListenObjects.serverObject).toBeInstanceOf(Object);
   });
+
+  test('the listen object is truthy', () => {
+    expect(serverAndListenObjects.listenObject).toBeTruthy();
+  });
+
+  test('the listen object is an object', () => {
+    expect(typeof serverAndListenObjects.listenObject).toBe("object");
+  });
+
   test("the port used is the chosen port", () => {
     expect(usedPort).toBe(chosenPort.toString());
   });
+
   test("plain/homepage GET request returns a valid 200 response", async () => {
     const response = await axios.get("http://localhost:" + usedPort);
     expect(response.status).toBe(200);
   });
+
   test("random invalid GET request returns a 404 response", async () => {
     await expect(axios.get("http://localhost:" + usedPort + "/" + chosenPort.toString())).rejects.toBeInstanceOf(Object);
   });
+  
   test ("plain/homepage GET request response is doctype html", async() => { 
     const response = await axios.get("http://localhost:" + usedPort);
     expect( response.data.includes(`<!doctype html>`) ).toBe(true);
