302a689dc2b575e224c789c68b60f82791a616eb
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-11-12 16:05:44 -0500

    fix: aligned homepage(coming soon) with color theme

--- src/pages/ComingSoon/ComingSoon.jsx
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -1,10 +1,12 @@
 import "./ComingSoon.css";
-import logo from '../../images/logo.svg';
+import logo from "../../images/logo.svg";
 
 export default function ComingSoon() {
   return (
     <div className="ComingSoon">
-      <p className="coming-soon">Coming Soon!</p>
+      <div className="top-banner">
+        <p>Coming Soon!</p>
+      </div>
       <header className="ComingSoon-header">
         <img src={logo} className="ComingSoon-logo" alt="logo" />
         <p>GitToKnowEachOther</p>
