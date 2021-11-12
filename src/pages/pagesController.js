cdca619b8079770d36126379f061d0eefa8d5edf
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-11-11 20:33:25 -0500

    del: removed exploratory code

--- src/pages/pagesController.js
+++ src/pages/pagesController.js
@@ -1,15 +1,6 @@
 import ComingSoon from "./ComingSoon/ComingSoon.jsx";
-import Login from "./Login/Login.jsx";
-import Settings from "./Settings/Settings.jsx";
 
-export function getAllPages() {
-  return [<ComingSoon />, <Login />, <Settings />];
-}
 
-export function getAllPagesWithRoutes() {
-  return [
-    <Route path="/" component={ComingSoon} />,
-    <Route path="/login" component={Login} />,
-    <Route path="/settings" component={Settings} />,
-  ];
+export function getHomepage(){ 
+  return <ComingSoon />
 }
