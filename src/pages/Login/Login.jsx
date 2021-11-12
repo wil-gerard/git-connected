148cba86e59ce5b2d9a0f4b3dc3a1fd7e16bd8c4
FrostyyPaws <90424167+FrostyyPaws@users.noreply.github.com>
2021-11-12 17:49:01 -0500

    add: username and password are inputs, input styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,33 +1,30 @@
 import "./Login.css";
-import logo from "../../images/logo.svg"
-import twitterLogo from "../../images/twitter.svg"
-import googleLogo from "../../images/google.svg"
-import facebookLogo from "../../images/facebook.svg"
+import logo from "../../images/logo.svg";
+import twitterLogo from "../../images/twitter.svg";
+import googleLogo from "../../images/google.svg";
+import facebookLogo from "../../images/facebook.svg";
 
 export default function Login() {
   return (
     <div className="Login">
-
       <img src={logo} className="logo" alt="logo" />
-      <div className = "title">
+      <div className="title">
         <p>Sign in</p>
       </div>
-      <div className = "username">
-        <p>Username</p>
+      <div className="username">
+        <input autoFocus={true} maxLength="100" placeholder="Username" />
       </div>
-      <div className = "password">
-        <p>Password</p>
+      <div className="password">
+      <input type="password" maxLength="100" placeholder="Password" />
       </div>
-      <div className = "login-button">
+      <div className="login-button">
         <p>Sign in</p>
       </div>
-      <div className = "social-icons">
-      <img src={twitterLogo} className="twitter-logo" alt="twitter logo" />
-      <img src={googleLogo} className="google-logo" alt="google logo" />
-      <img src={facebookLogo} className="facebook-logo" alt="facebook logo" />
+      <div className="social-icons">
+        <img src={twitterLogo} className="twitter-logo" alt="twitter logo" />
+        <img src={googleLogo} className="google-logo" alt="google logo" />
+        <img src={facebookLogo} className="facebook-logo" alt="facebook logo" />
       </div>
-
-
     </div>
   );
 }
