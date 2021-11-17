5642b31c60364f42d350b76abb5c60582f66a32e
Wil Gerard <williamrgerard@gmail.com>
2021-11-17 16:00:27 -0600

    add: basic structure for profile with navbar and footer components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <div>
-            
+            <h1>Navbar</h1>
         </div>
     )
 }
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
+export default function Profile() {
+    return (
+        <>
+            <Navbar />
+            <div>
+                <h1>Profile</h1>
+            </div>
+            <Footer />
+        </>
+    )
+}
