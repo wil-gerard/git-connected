5a0422be9bbb368bab69ac484f47621676dbb89e
Wil Gerard <williamrgerard@gmail.com>
2021-11-24 20:28:47 -0600

    add: started creating a new login page using Tailwind

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import "./Login.css";
+// import "./Login.css";
 import logo from "../../images/logo.svg";
 import twitterLogo from "../../images/twitter.svg";
 import googleLogo from "../../images/google.svg";
@@ -6,24 +6,13 @@ import facebookLogo from "../../images/facebook.svg";
 
 export default function Login() {
   return (
-    <div className="Login">
-      <img src={logo} className="logo animate-spin" alt="logo" />
-      <div className="title">
-
-      </div>
-      <div className="username">
-        <input autoFocus={true} maxLength="100" placeholder="Username" />
-      </div>
-      <div className="password">
-      <input type="password" maxLength="100" placeholder="Password" />
-      </div>
-      <div className="login-button">
-        <p>Sign in</p>
-      </div>
-      <div className="social-icons">
-        <img src={twitterLogo} className="twitter-logo" alt="twitter logo" />
-        <img src={googleLogo} className="google-logo" alt="google logo" />
-        <img src={facebookLogo} className="facebook-logo" alt="facebook logo" />
+    <div className="flex justify-center items-center h-screen">
+      <div className="max-w-sm rounded shadow-lg">
+        <div className="px-6 py-4 flex-col flex">
+          <button className="rounded-lg text-sm px-5 py-2.5 text-center mr-3 mb-3 border-blue-darkest hover:bg-blue-darker focus:ring-blue-darker">Login</button>
+          <button className="rounded-lg text-sm px-5 py-2.5 text-center mr-3 mb-3 border-blue-darkest hover:bg-blue-darker focus:ring-blue-darker">Login</button>
+          <button className="rounded-lg text-sm px-5 py-2.5 text-center mr-3 mb-3 border-blue-darkest hover:bg-blue-darker focus:ring-blue-darker">Login</button>
+        </div>
       </div>
     </div>
   );
