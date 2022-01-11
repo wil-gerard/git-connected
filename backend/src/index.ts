40450562f44d647ad2676167f1d697284ca1e3b8
Wil Gerard <williamrgerard@gmail.com>
2022-01-11 16:09:05 -0600

    add: env PORT variable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -150,6 +150,8 @@ app.get("/auth/logout", (req, res) => {
     }
 })
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
     console.log("server started")
 })
