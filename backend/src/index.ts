cf69a93eb2425f97d9c8f69da0a65f1d6cdd6834
Wil Gerard <williamrgerard@gmail.com>
2022-03-10 14:09:02 -0600

    client directory now inside backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -64,9 +64,9 @@ import './config/database';
 
 // Production Deploy
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 }
 
