ffd2128c6425887160717295b5c2795015573b0e
Wil Gerard <74286884+wil-gerard@users.noreply.github.com>
2022-03-10 14:38:35 -0600

    Prod restructure (#102)
    * built
    
    * chore: netlify redirect
    
    * chore: moved _redirects
    
    * chore: create dev and production env variables client-side
    
    * refactor: clean unneccesary code
    
    * chore: netlify toml settings
    
    * chore: netlify cleanup
    
    * chore: move toml
    
    * chore: toml build command
    
    * toml config
    
    * chore: monorepo toml set up
    
    * test: console logs for server/mongo
    
    * client directory now inside backend
    
    * fix: client variable conflict
    
    * Delete netlify.toml
    
    * fix: prod deploy path resolve

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -66,7 +66,7 @@ import './config/database';
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, "clien", "build", "index.html"));
   });
 }
 
