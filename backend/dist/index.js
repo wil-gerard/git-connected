b9db369c13133b092d692faba9a06b40f002a144
Wil Gerard <74286884+wil-gerard@users.noreply.github.com>
2022-03-09 23:15:52 -0600

    New heroku build (#90)
    * fix: profile update
    
    * procfile
    
    * heroku build

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var dotenv_1 = __importDefault(require("dotenv"));
+// import dotenv from 'dotenv';
 var path_1 = __importDefault(require("path"));
-dotenv_1.default.config({ path: __dirname + "/../.env" });
+// dotenv.config( { path: __dirname+"/../.env"} );
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 var morgan_1 = __importDefault(require("morgan"));
