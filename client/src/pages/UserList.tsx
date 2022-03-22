69bf2c83ad6c83ce99787185ca4bb181c3036ebc
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-03-21 21:27:19 -0400

    sort methodology changed

--- client/src/pages/UserList.tsx
+++ client/src/pages/UserList.tsx
@@ -99,7 +99,9 @@ export default function Home() {
   }, []);
 
   users?.sort((a: IUser, b: IUser) => {
-    return a.gitHub.json.name.localeCompare(b.gitHub.json.name);
+    if ( alreadyFollowing[a._id] ) { return 1 }
+    else if (alreadyFollowing[b._id]) { return -1 }
+    else { return 0 }
   });
 
   return (
