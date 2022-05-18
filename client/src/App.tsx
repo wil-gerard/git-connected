d2cdfee08f93d465a78c96f566acabaf0ed0bfca
Castle <castlerock88@protonmail.com>
2022-05-18 19:39:09 -0400

    public profile page
    create public profile page
    fetch queried user data from api
    populate UserCard.tsx with queriedUser data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Profile from './pages/Profile';
+import PublicProfile from './pages/PublicProfile';
 import Profiles from './pages/Profiles';
 import Featured from './pages/Featured';
 import Home from './pages/Home';
@@ -20,6 +21,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/profiles" element={<Profiles />} />
               <Route path="/featured" element={<Featured />} />
+              <Route path="/publicprofile/:id" element={<PublicProfile />} />
               <Route
                 path="/profile"
                 element={
