5bd58e94fefc5b80bd723bfbd62f55b6c8bca12e
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-06-23 20:26:54 -0400

    add cursor pointer to FooterLink (#191)

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,7 +6,7 @@ const FooterContainer = tw.footer`container mx-auto px-6 pt-10`;
 const Column = tw.div`mt-8 mb-16 sm:mt-0 sm:w-full sm:px-8 lg:px-40 flex flex-col md:flex-row justify-between`;
 const FooterContent = tw.div`flex flex-col`;
 const FooterContentHeader = tw.h4`font-bold text-gray-300 uppercase mb-2`;
-const FooterLink = tw.span`my-2 text-gray-300 text-base hover:text-primary-500 transition duration-300`;
+const FooterLink = tw.span`my-2 text-gray-300 text-base hover:text-primary-500 transition duration-300 cursor-pointer`;
 
 export default function Footer() {
   const navigate = useNavigate();
